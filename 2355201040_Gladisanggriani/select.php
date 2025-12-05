242d49bea09f4eda2efb3fe75778727dc3cd4945
Gladis <gladisanggraini4@gmail.com>
2025-12-05 10:59:07 +0700

    select.php

---
+++ 2355201040_Gladisanggriani/select.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if($_SERVER['REQUEST_METHOD'] != 'GET'){
+    http_response_code(405);
+    $res = [
+        'status' => 'error',
+        'msg' => 'Method Salah'
+    ];
+    echo json_encode($res);
+    exit();
+}
+
+$koneksi = mysqli_connect("localhost","root","","uts_be");
+$q = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
+$data = [];
+while($row = mysqli_fetch_assoc($q)){
+    $data[] = $row;
+}
+
+if(count($data) == 0){
+    http_response_code(404);
+    $res = [
+        'status' => 'error',
+        'msg' => 'Data not found'
+    ];
+    echo json_encode($res);
+    exit();
+}
+
+http_response_code(200);
+$res = [
+    'status' => 'succcess',
+    'msg' => 'Process success',
+    'data' => $data
+];
+echo json_encode($res);
